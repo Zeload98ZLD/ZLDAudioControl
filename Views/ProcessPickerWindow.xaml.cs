976300d49bee65db453c82ccfe19eda63335ee7d
Zeload <zeloadbuisness@gmail.com>
2026-07-15 13:44:39 +0200

    Fix missing System.IO import

--- Views/ProcessPickerWindow.xaml.cs
+++ Views/ProcessPickerWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -109,3 +110,4 @@ public partial class ProcessPickerWindow : Window
         RoutedEventArgs e) =>
         DialogResult = false;
 }
+
